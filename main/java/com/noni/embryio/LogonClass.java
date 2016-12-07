801180cfde7073d5711c71e76c35fdd9a1eaa85f
Harsh Overseer <harsh@embry.io>
2016-12-08 09:26:45 +1100

    also removing view elements for logon stuff

--- main/java/com/noni/embryio/LogonClass.java
+++ main/java/com/noni/embryio/LogonClass.java
@@ -1,27 +1,7 @@
 package com.noni.embryio;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import org.apache.http.Header;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-
 import android.app.ActionBar;
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -30,217 +10,163 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.exception.DropboxException;
+import com.dropbox.client2.session.AppKeyPair;
 
 
 public class LogonClass extends FragmentActivity implements OnClickListener {
 
-	private SharedPreferences prefs;
-	private String TAG = "LogonClass.java";
-	private EditText passwordText, usernameText;
-	private String usernameString;
-	private String passwordString;
-	private Button logonButton, resetButton;
-	private TextView logonstatus;
-	private final String LOGON_URL   = Constants.SERVERURL+ "login_mobile";
-	int TIMEOUT_MILLSEC = 10000;
-	public static MyHttpClient Client = new MyHttpClient(null);
-	public ProgressDialog mProgressDialog;
-	private ActionBar actionBar;
-	
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		actionBar = getActionBar();
-		actionBar.hide();
-		setContentView(R.layout.logonview);
-		usernameText = (EditText)findViewById(R.id.username);
-		passwordText = (EditText)findViewById(R.id.password);
-		logonButton = (Button)findViewById(R.id.logonbutton);
-		resetButton = (Button)findViewById(R.id.resetbutton);
-		logonButton.setOnClickListener(this);
-		resetButton.setOnClickListener(this);
-		
-	}
-	
-	@Override
-	protected void onDestroy()
-	{
-		Log.e(TAG, "On Destroy called");
-		super.onDestroy();
-	}
-	
-	@Override
-	protected void onPause()
-	{
-		Log.e(TAG, "On Pause called");
-		super.onPause();
-	}
+    private SharedPreferences prefs;
+    private String TAG = "LogonClass.java";
+    private Button logonButton;
+    private TextView logonstatus;
+    private final String LOGON_URL = Constants.SERVERURL + "login_mobile";
+    int TIMEOUT_MILLSEC = 10000;
+    public static MyHttpClient Client = new MyHttpClient(null);
+    private ProgressDialog mProgressDialog;
+    private ActionBar actionBar;
+    private final static AppKeyPair emboAppKeys = new AppKeyPair(Constants.API_KEY, Constants.API_KEY);
+    private DropboxAPI<AndroidAuthSession> emboDBApi;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidAuthSession newSession = new AndroidAuthSession(emboAppKeys);
+        emboDBApi = new DropboxAPI<>(newSession);
+        actionBar = getActionBar();
+        actionBar.hide();
+        setContentView(R.layout.logonview);
+        logonButton = (Button) findViewById(R.id.logonbutton);
+        logonButton.setOnClickListener(this);
+        prefs = this.getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString("emboDBAccessToken", "");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(TAG, "On Destroy called");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(TAG, "On Pause called");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (emboDBApi.getSession().authenticationSuccessful()) {
+            try {
+                // Required to complete auth, sets the access token on the session
+                emboDBApi.getSession().finishAuthentication();
+
+                String accessToken = emboDBApi.getSession().getOAuth2AccessToken();
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("emboDBAccessToken", accessToken);
+                editor.apply();
+                Log.v("SomeTag", prefs.getString())
+            } catch (IllegalStateException e) {
+                Log.i("DbAuthLog", "Error authenticating", e);
+            }
+        }
+    }
 
 
-	@Override
-	public  void onBackPressed()
-	{
-		finish();
-	}
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
 
-	@Override
-	public void onClick(View view) {
-	// TODO Auto-generated method stub
-		HttpMethodTask HMT = new HttpMethodTask(getApplicationContext());
-		switch (view.getId())
-		{	
-			case R.id.logonbutton:
-			{
-				Log.e(TAG, "logon button pressed");
-				usernameString = usernameText.getText().toString();
-				passwordString = passwordText.getText().toString();
-				Log.e(TAG, "username is " + usernameString + "password is " + passwordString);
-				if (HMT.getStatus() != AsyncTask.Status.RUNNING)
-				{
-					try
-					{
-						HMT.execute(LOGON_URL);
-						//new HttpMethodTask(getApplicationContext()).execute(testURL);
-					}
-					catch (Exception e)
-					{
-						e.printStackTrace();
-					}
-				}
-				break;
-			}
-				
-			case R.id.resetbutton:
-			{
-				Intent i = new Intent(this, Register_New.class);
-				startActivity(i);
-				finish();
-				break;
-			}
-		}
-	}
-	
-public class HttpMethodTask extends AsyncTask<String, Void, String> {
-		
-		public Context context;
+    @Override
+    public void onClick(View view) {
+        // TODO Auto-generated method stub
+        switch (view.getId()) {
+            case R.id.logonbutton: {
+                Log.e(TAG, "logon button pressed");
+                if (!checkIfDBUnlinked()) {
+                    Log.v(TAG, "Db unlinked eh");
+                    startSessionWhenUnlinked(this);
+                } else {
+                    //retrieve token and build session
+                    Log.v(TAG, "db still linked");
+                    AndroidAuthSession newSession = new AndroidAuthSession(emboAppKeys, prefs.getString("emboDBAccessToken", ""));
+                    //build API
+                    emboDBApi = new DropboxAPI<>(newSession);
 
-		public HttpMethodTask(Context ApplicationContext) {
-			// TODO Auto-generated constructor stub
-			this.context = ApplicationContext;
-			
-		}
-		
-		@Override
-		protected void onPreExecute()
-		{
-			super.onPreExecute();
-			mProgressDialog = new ProgressDialog(LogonClass.this);
-			mProgressDialog.setProgress(ProgressDialog.STYLE_SPINNER);
-			mProgressDialog.setTitle("Logging you in");
-			mProgressDialog.setMessage("one sec, beautiful ;)");
-			mProgressDialog.setCancelable(false);
-			mProgressDialog.setIndeterminate(true);
-			mProgressDialog.show();
-		}
+                    FileOperations fp = new FileOperations(getApplicationContext(), emboDBApi);
+                    fp.execute("test");
+                }
+                break;
+            }
+        }
+    }
 
-		@Override
-		protected String doInBackground(String... url) {
-			// TODO Auto-generated method stub
-			//first get u/p prefs, store and test;
-			String fleh = "no response?";
-			//create a http post request
-			Header[] responseHeaders = null;
-			HttpParams httpParams = new BasicHttpParams();
-			HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLSEC);
-			HttpPost request = new HttpPost(url[0]);
-			ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-			nameValuePairs.add(new BasicNameValuePair("login", "True"));
-			nameValuePairs.add(new BasicNameValuePair("username", usernameString));
-		    nameValuePairs.add(new BasicNameValuePair("password", passwordString));
-			try
-			{
-			   UrlEncodedFormEntity entity = new UrlEncodedFormEntity(nameValuePairs);
-			   request.setEntity(entity);
-			   Client.putContext(context);
-			   HttpResponse response = Client.execute(request);
-			   fleh = EntityUtils.toString(response.getEntity());
-			   Log.e(TAG, "fleh returned "+ fleh);
-			   responseHeaders = response.getAllHeaders();
-			   for (int i=0; i<responseHeaders.length; i++)
-				{
-					Header h = responseHeaders[i];
-					String name = h.getName();
-					String value = h.getValue();
-					Log.e(TAG, name + " " + value);
-				}
-			
-			}
-			catch (ClientProtocolException e)
-				{
-				Log.e(TAG, "client protocol exception!");				
-				} 
-			catch (IOException e)
-			{
-				e.printStackTrace();
-			}
-			catch (Exception e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			Log.e(TAG, "fleh returned " + fleh);
-			return fleh;
-		}
-		
-		
-		@Override
-		protected void onPostExecute(String s)
-		{
-			if (mProgressDialog != null)
-			{
-				mProgressDialog.dismiss();
-			}
-			
-			if (s.equals("no response?"))
-			{
-				Toast.makeText(LogonClass.this, "No connection to the internet detected :(", Toast.LENGTH_SHORT).show();
-			}
-			else
-			{
-				String status = null, error = null;	
-				JSONTokener tokener = new JSONTokener(s);
-			
-				try {
-					JSONObject response = (JSONObject) tokener.nextValue();
-					status = response.getString("status");
-					 error = response.getString("error");
-					 Log.e(TAG, "status " + status + " error " + error);
-					
-					 if (error.equals("none"))
-					 {
-						Intent i = new Intent(LogonClass.this, MainActivity.class);
-						startActivity(i);
-						finish();
-					 }
-					 else
-					 {
-						 Toast.makeText(LogonClass.this, error, Toast.LENGTH_SHORT).show();
-					 }
-					 
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				catch (java.lang.ClassCastException e )
-				{
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+    public boolean tokenExists() {
+        Log.v(TAG, "checking that token exists");
+
+        if (prefs.getString("emboDBAccessToken", "").equals("")) {
+            Log.v(TAG, "Nah token don't exist bruv");
+            return false;
+        } else {
+            Log.v(TAG, "Yah token exists");
+            return true;
+        }
+    }
+
+    public void startSessionWhenUnlinked(Context context) {
+        emboDBApi.getSession().startOAuth2Authentication(context);
+    }
+
+    public boolean checkIfDBUnlinked() {
+        AndroidAuthSession newSession = new AndroidAuthSession(emboAppKeys, prefs.getString("emboDBAccessToken", ""));
+        emboDBApi = new DropboxAPI<AndroidAuthSession>(newSession);
+
+        if (!tokenExists()) {
+            return false;
+        }
+        return verifyAuth();
+    }
+
+    private boolean verifyAuth() {
+        VerifyAuthing va = new VerifyAuthing(emboDBApi);
+        if (va.execute().equals("")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public class VerifyAuthing extends AsyncTask<Void, Void, String> {
+
+        private DropboxAPI<AndroidAuthSession> dbApi;
+
+        public VerifyAuthing(DropboxAPI<AndroidAuthSession> dbApi) {
+            this.dbApi = dbApi;
+        }
+
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String response = "";
+            try {
+                DropboxAPI.Account account = dbApi.accountInfo();
+                response = account.displayName;
+            } catch (DropboxException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+    }
 }
+
+
+
 
