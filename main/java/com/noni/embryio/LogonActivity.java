1ed4be1c4003224de88a7f2a9235548a7f5d5c29
Harsh Overseer <harsh@embry.io>
2016-12-29 09:16:12 +1100

    added terms and conditions + why dropbox links to logon page.

--- main/java/com/noni/embryio/LogonActivity.java
+++ main/java/com/noni/embryio/LogonActivity.java
@@ -1,37 +1,76 @@
 package com.noni.embryio;
 
-import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.text.SpannableString;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.android.AndroidAuthSession;
 
 
-public class LogonClass extends FragmentActivity implements OnClickListener {
+public class LogonActivity extends FragmentActivity implements OnClickListener {
 
     private SharedPreferences prefs;
     private final String TAG = this.getClass().getSimpleName();
     private DropboxAPI<AndroidAuthSession> emboDBApi;
     private AndroidAuthSession newSession = new AndroidAuthSession(Constants.KEY_PAIR);
     private Boolean buttonPressed = false;
+    private final String termsURL = "https://poppito.github.io/terms";
+    private final String whyDropboxURL = "https://poppito.github.io/whyDropbox";
+    private final String licenseSpan = "License Terms";
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         emboDBApi = new DropboxAPI<>(newSession);
-        ActionBar actionBar = getActionBar();
-        actionBar.hide();
         setContentView(R.layout.logonview);
         Button logonButton = (Button) findViewById(R.id.logonbutton);
+        TextView whyDropboxView = (TextView) findViewById(R.id.explainWhy);
+        initialiseClickableSpan(this, whyDropboxView, whyDropboxURL, getResources().getString(R.string.whyDropbox));
+        TextView licenseAgreementView = (TextView) findViewById(R.id.licenseAgreement);
+        initialiseClickableSpan(this, licenseAgreementView, termsURL, licenseSpan);
         logonButton.setOnClickListener(this);
         prefs = this.getSharedPreferences("prefs", MODE_PRIVATE);
+    }
+
+    private void initialiseClickableSpan(final Context context, TextView tv, final String url, String spannedString) {
+
+        SpannableString span = new SpannableString(tv.getText().toString());
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(View widget) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                context.startActivity(browserIntent);
+            }
+
+            @Override
+            public void updateDrawState(TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true);
+            }
+        };
+
+
+        int[] bounds = getIndexOfUrlKeywords(spannedString, tv.getText().toString());
+        if (bounds != null && bounds[0] >= 0) {
+            span.setSpan(clickableSpan, bounds[0], bounds[1], SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        tv.setText(span);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 
     @Override
@@ -120,6 +159,14 @@ public class LogonClass extends FragmentActivity implements OnClickListener {
             Log.v(TAG, e.getMessage());
         }
         return false;
+    }
+
+
+    private int[] getIndexOfUrlKeywords(String keyword, String containingString) {
+        int[] index = new int[2];
+        index[0] = containingString.indexOf(keyword);
+        index[1] = index[0] + keyword.length();
+        return index;
     }
 }
 
